81e15d26cc231cb0690062672f8a7f8c713622ac
Marcelino <marconaoum@gmail.com>
2025-05-25 17:18:03 +0300

    Adpater Pattern

--- src/AdapterPattern/MainClient.java
+++ src/AdapterPattern/MainClient.java
@@ -1,12 +1,6 @@
 package AdapterPattern;
 
-/*Adapter Pattern:
-Convert the interface of a class into another interface clients expect.
-Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
-To simplify how it works:
-    similar to real life adapter,when you plug in the cable(iTarget) of 3line to convert it to 2 lines you use the adapter
-    our Adapter divide into (internal process (adapter class) / the 3 line interface(adaptee Class) )
-* */
+
 public class MainClient {
     public static void main(String[] args) {
         USPlugIn US = new USPlugIn();
